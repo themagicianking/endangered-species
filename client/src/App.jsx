f1cd626e456cb9d6b8fd258a8b9d9e7e2976538b
Thane Chaves <tmoonder@gmail.com>
2024-09-17 21:43:15 -0400

    Added additional mui dependencies and Tabs component.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Tabs from "./components/Tabs";
 
 function App() {
   return (
     <>
       <h1>Thane's Endangered Species Tracker</h1>
+      <Tabs />
     </>
   );
 }
