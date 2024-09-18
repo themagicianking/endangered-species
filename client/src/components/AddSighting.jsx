f7e41efa5940f138f5418be60583713cf90afea9
Thane Chaves <tmoonder@gmail.com>
2024-09-17 23:04:19 -0400

    Finished basic form functionality for adding a sighting.

--- client/src/components/AddSighting.jsx
+++ client/src/components/AddSighting.jsx
@@ -1,20 +1,35 @@
 export default function AddSighting() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const SIGHTING = {
+      date: event.target.date.value,
+      time: event.target.time.value,
+      animalName: event.target["animal-name"].value,
+      location: event.target.location.value,
+      healthy: event.target.healthy.checked,
+      email: event.target.email.value,
+      timestamp: Date.now(),
+    };
+    console.log(SIGHTING);
+
+    event.target.reset();
+  };
+
   return (
-    <form>
-      {/* integer primary key */}
-      <label for="date">Date</label>
-      <input id="date" type="date"></input>
-      <label for="time">Time</label>
-      <input id="time" type="time"></input>
-      <label for="animal-name">Animal Name</label>
-      <input id="animal-name" type="text"></input>
-      <label for="location">Location</label>
-      <input id="location" type="text"></input>
-      <label for="health">Healthy?</label>
-      <input id="health" type="checkbox"></input>
-      <label for="email">Email</label>
-      <input id="email" type="email"></input>
-      {/* record creation timestamp */}
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="date">Date</label>
+      <input id="date" name="date" type="date"></input>
+      <label htmlFor="time">Time</label>
+      <input id="time" name="time" type="time"></input>
+      <label htmlFor="animal-name">Animal Name</label>
+      <input id="animal-name" name="animal-name" type="text"></input>
+      <label htmlFor="location">Location</label>
+      <input id="location" name="location" type="text"></input>
+      <label htmlFor="healthy">Healthy?</label>
+      <input id="healthy" name="healthy" type="checkbox"></input>
+      <label htmlFor="email">Email</label>
+      <input id="email" name="email" type="email"></input>
+      <button type="submit">Create Sighting</button>
     </form>
   );
 }
